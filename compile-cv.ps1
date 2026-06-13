506977f64335058dc6fc1baae094d9ee2936a38f
Haobo Zhao <haoboamericasecret@outlook.com>
2026-06-13 14:05:32 -0400

    fix

--- compile-cv.ps1
+++ compile-cv.ps1
@@ -2,9 +2,9 @@ $ErrorActionPreference = "Stop"
 
 $root = Split-Path -Parent $MyInvocation.MyCommand.Path
 $miktex = Join-Path $env:LOCALAPPDATA "Programs\MiKTeX\miktex\bin\x64"
-$perl = "C:\Strawberry\perl\bin;C:\Strawberry\c\bin"
 
-$env:Path = "$miktex;$perl;$env:Path"
+$env:Path = "$miktex;$env:Path"
 Set-Location (Join-Path $root "CV")
 
-latexmk -pdf -interaction=nonstopmode -file-line-error CV_Haobo_Zhao_2024.tex
+pdflatex -interaction=nonstopmode -file-line-error CV_Haobo_Zhao_2024.tex
+pdflatex -interaction=nonstopmode -file-line-error CV_Haobo_Zhao_2024.tex
